01648adb9e5a5fa2efe82ebf9bc7fa5d8e436a1d
LuluBeatson <lulu.beatson@gmail.com>
2023-05-14 18:02:02 +0100

    use port flag

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Simple HTTP Server in Go
 
-https://www.youtube.com/watch?v=ASBUp7stqjo
+This is a simple HTTP server written in Go. It is based on the [tutorial](https://www.youtube.com/watch?v=ASBUp7stqjo) by Akhil Sharma.
+
+# Start
+
+```bash
+go build
+go run main.go
+```
+Starts a server on port 8080. To use a different port:
+
+```bash
+go run main.go -port=5000
+```
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+var port int
+
+func init() {
+	flag.IntVar(&port, "port", 8080, "port to listen on")
+	flag.Parse()
+}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
@@ -12,7 +20,8 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/form", formHandler)
 
-	fmt.Printf("Starting server at port 8080\n")
+	fmt.Println("Starting server at port", port)
+	fmt.Printf("http://localhost:%v/", port)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
